eeb3537aaa9dc59c19e8f976a1fc9aa6abe3e2c8
Jana Jammoul <jammouljana182@gmil.com>
2026-09-09 20:23:00 +0300

    update the user model add tow attributes password and role and install libraries prepartion for authentication

--- model/userModel.js
+++ model/userModel.js
@@ -1,22 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required: true,
-        
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    address:{
-        type:String,
-        required: true,
-        
-    }
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-})
-
-const User = mongoose.model("User ", userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
