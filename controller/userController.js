5554ea26532841366fab7b636c7eda9142dd9eae
Jana Jammoul <jammouljana182@gmil.com>
2026-09-09 15:25:15 +0300

    build getUserById api

--- controller/userController.js
+++ controller/userController.js
@@ -30,3 +30,18 @@ exports.getAllUsers=async(req ,res)=>{
         res.status(500).json({message:error.message})
 }
 }
+
+
+exports.getUserById=async(req,res)=>{
+  try{
+    const userId=req.params.id;
+    const user = await User.findById(userId)
+    if(!user){
+      return res.status(404).json({message:"User not found"})
+    }
+    res.status(200).json(user)
+    }
+   catch(error){
+        res.status(500).json({message:error.message})
+}
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express =require('express');
-const {createUser,getAllUsers} = require('../controller/userController');
+const {createUser,getAllUsers,getUserById} = require('../controller/userController');
 const router = express.Router();
 
 router.post('/users', createUser);
 router.get('/users',getAllUsers)
-
+router.get('/users/:id',getUserById)
 module.exports = router;
