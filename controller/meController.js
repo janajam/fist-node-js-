07eb3053188af1c86f33a37d9238d1c91b11ce76
Jana Jammoul <jammouljana182@gmil.com>
2026-09-10 21:03:37 +0300

    build updateMe api for update the data of current user

--- controller/meController.js
+++ controller/meController.js
@@ -14,3 +14,22 @@ exports.getMe=async(req,res)=>{
     return res.status(500).json({ message: error.message });
   }
 }
+
+
+exports.updateMe=async(req,res)=>{
+    try{
+        const {name,email}=req.body 
+        const id =req.user.id
+        const me=await User.findById(id)
+        if(!me){
+            return res.status(404).json({message:'data dose not found'})
+        }
+        const updatedMe=await User.findByIdAndUpdate(id,{name,email},{
+            new:true
+        })
+        return res.status(200).json({message:'data updated successfully',users:updatedMe})
+    }
+    catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
--- routes/meRoute.js
+++ routes/meRoute.js
@@ -1,8 +1,9 @@
 const express=require('express')
-const { getMe } = require('../controller/meController')
-const {verifyToken,authorizeRole}=require('../middleware/authMiddlewar')
+const { getMe ,updateMe} = require('../controller/meController')
+const {verifyToken}=require('../middleware/authMiddlewar')
 const meRouter=express.Router()
 
 meRouter.get('/me',verifyToken,getMe)
+meRouter.put('/me',verifyToken,updateMe)
 
 module.exports=meRouter
