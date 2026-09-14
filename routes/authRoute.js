8e3337c882175137cafab3c02c0ac39e48ee4944
Jana Jammoul <jammouljana182@gmil.com>
2026-09-14 19:37:45 +0300

    create validation middleware to handel validation errors

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,10 +5,12 @@ const {
   logout,
   refreshAccessToken,
 } = require("../controller/authController");
-const {verifyRefreshToken}=require('../middleware/authMiddlewar')
+const {verifyRefreshToken}=require('../middleware/authMiddlewar');
+const { registerValidator } = require("../validator/authValidator");
+const{validate}=require('../middleware/validationMiddleware')
 const authRouter = express.Router();
 
-authRouter.post("/register", register);
+authRouter.post("/register", registerValidator,validate,register);
 authRouter.post("/login", login);
 authRouter.post('/refresh',verifyRefreshToken,refreshAccessToken)
 authRouter.post(
